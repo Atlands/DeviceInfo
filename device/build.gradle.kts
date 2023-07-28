859a08b6a796f956854b57d30c3b5baba36d6704
Helios030 <tunnello@163.com>
2023-07-28 12:06:03 +0800

    init

--- device/build.gradle.kts
+++ device/build.gradle.kts
@@ -1,9 +1,16 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 group=("com.github.Helios030")
 
+// 创建一个task来发布源码
+tasks.register<Jar>("sourcesJar") {
+    archiveClassifier.set("sources")
+    val sources = android.sourceSets.map { set -> set.java.getSourceFiles() }
+    from(sources)
+}
 
 android {
     namespace = "com.qc.device"
@@ -12,6 +19,33 @@ android {
         minSdk = 24
     }
 
+    afterEvaluate {
+        publishing {
+            repositories {
+                maven {
+                    // isAllowInsecureProtocol = true // 如果Maven仓库仅支持http协议, 请打开此注释
+                    url = uri("https://github.com/Helios030/DeviceInfo.git") // 请填入你的仓库地址
+                    authentication {
+                        create<BasicAuthentication>("basic")
+                    }
+                    credentials {
+                        username = "Helios030" // 请填入你的用户名
+                        password = "Nb1300454585" // 请填入你的密码
+                    }
+                }
+            }
+
+            publications {
+                create<MavenPublication>("product") {
+                    from(components["release"])
+                    groupId = "DeviceInfo" // 请填入你的组件名
+                    artifactId = "DeviceInfo" // 请填入你的工件名
+                    version = "v1.0.0" // 请填入工件的版本名
+                    artifact(tasks["sourcesJar"]) // 打包源码到工件中
+                }
+            }
+        }
+    }
 
     buildTypes {
         release {
@@ -44,3 +78,10 @@ dependencies {
 
 
 }
+
+
+
+
+
+
+
